33d073f9b96fcd9c1b1c45f5bfc0153435104bb8
Wellington Adriano Nicoletti de Abreu <wellingtonicoletti@gmail.com>
2023-09-22 19:15:50 -0300

    refactor: ajuste no width do ícone de notificação padrão

--- wellnotify.js
+++ wellnotify.js
@@ -583,7 +583,7 @@ class WellNotify {
     warning:
       '<svg viewBox="0 0 24 24" width="100%" height="100%" fill="var(--wellnotify-icone-color)"><path d="M23.32 17.191L15.438 2.184C14.728.833 13.416 0 11.996 0c-1.42 0-2.733.833-3.443 2.184L.533 17.448a4.744 4.744 0 000 4.368C1.243 23.167 2.555 24 3.975 24h16.05C22.22 24 24 22.044 24 19.632c0-.904-.251-1.746-.68-2.44zm-9.622 1.46c0 1.033-.724 1.823-1.698 1.823s-1.698-.79-1.698-1.822v-.043c0-1.028.724-1.822 1.698-1.822s1.698.79 1.698 1.822v.043zm.039-12.285l-.84 8.06c-.057.581-.408.943-.897.943-.49 0-.84-.367-.896-.942l-.84-8.065c-.057-.624.25-1.095.779-1.095h1.91c.528.005.84.476.784 1.1z"></path></svg>',
     loading: '<div class="wellnotify_IconeLoading"><span></span></div>',
-    default: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="var(--wellnotify-icone-color)" class="bi bi-bell-fill" viewBox="0 0 16 16">
+    default: `<svg xmlns="http://www.w3.org/2000/svg" width="100%" fill="var(--wellnotify-icone-color)" class="bi bi-bell-fill" viewBox="0 0 16 16">
     <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
   </svg>`,
     close:
